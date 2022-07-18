8d15bc3864ae2536a549d836dd0101a51688a662
Matheus Rodrigues <matheusrf96@gmail.com>
2022-07-18 11:37:46 -0300

    Change values from inputs

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,38 +3,56 @@ import Button from "../Button";
 
 import style from './Form.module.scss'
 
-const Form = () => (
-    <form action="" className={style.novaTarefa}>
-        <div className={style.inputContainer}>
-            <div>
-                <label htmlFor="task">Task: </label>
-                <input
-                    type="text"
-                    name="task"
-                    id="task"
-                    placeholder="What do you want to do?"
-                    required
-                />
-            </div>
-        </div>
+class Form extends React.Component {
+    state = {
+        task: '',
+        time: '00:00:00'
+    }
 
-        <div className={style.inputContainer}>
-            <div>
-                <label htmlFor="time">Time: </label>
-                <input
-                    type="time"
-                    step="1"
-                    name="time"
-                    id="time"
-                    min="00:00:00"
-                    max="03:00:00"
-                    required
-                />
-            </div>
-        </div>
+    saveTask(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        console.log(this.state)
+    }
 
-        <Button text="Adicionar" />
-    </form>
-);
+    render() {
+        return (
+            <form action="" className={style.novaTarefa} onSubmit={this.saveTask.bind(this)}>
+                <div className={style.inputContainer}>
+                    <div>
+                        <label htmlFor="task">Task: </label>
+                        <input
+                            type="text"
+                            name="task"
+                            id="task"
+                            placeholder="What do you want to do?"
+                            required
+                            value={this.state.task}
+                            onChange={e => this.setState({ ...this.state, task: e.target.value })}
+                        />
+                    </div>
+                </div>
+
+                <div className={style.inputContainer}>
+                    <div>
+                        <label htmlFor="time">Time: </label>
+                        <input
+                            type="time"
+                            step="1"
+                            name="time"
+                            id="time"
+                            min="00:00:00"
+                            max="03:00:00"
+                            required
+                            value={this.state.time}
+                            onChange={e => this.setState({ ...this.state, time: e.target.value })}
+                        />
+                    </div>
+                </div>
+
+                <Button text="Adicionar" />
+            </form>
+        )
+    }
+}
 
 export default Form;
